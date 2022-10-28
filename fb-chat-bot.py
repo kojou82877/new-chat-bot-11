08a20b8e2c0c37073ac3412b860197d0a3a17027
kojou82877 <116699713+kojou82877@users.noreply.github.com>
2022-10-28 20:00:11 +0530

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -371,7 +371,7 @@ cookies = {
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
     "c_user": "100009784575461",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": "8%3AejyxqQF-ADDFoA%3A2%3A1666966549%3A-1%3A4799"
+    "xs": "8%3AejyxqQF-ADDFoA%3A2%3A1666966549%3A-1%3A4799%3A%3AAcVUJQ3NW-_n7VcO9fVmuIBaxjHeVHOfn92ZpxSvWQ"
 }
 
 
